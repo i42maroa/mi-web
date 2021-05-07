177d6701bc37260b148290751925f37ca4209ad7
i42maroa <i42maroa@uco.es>
2021-05-07 20:25:56 +0200

    Mejora archivo javascript

--- js/cafe-menu-animation.js
+++ js/cafe-menu-animation.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const cafe_boton = document.querySelector(".navbar-cafe");
 const imagen_boton = document.querySelector(".gota-agua");
 
@@ -8,7 +10,6 @@ const cafeHechoTitulo = document.querySelector(".navbar-menu-cafe__parte-dinero 
 const cafeIzq = document.querySelector(".navbar-menu-cafe__parte-dinero__pantalla-cambIzq");
 const cafeDer = document.querySelector(".navbar-menu-cafe__parte-dinero__pantalla-cambDer");
 
-
 const section_sobre = document.querySelector("#section-sobre");
 const section_sobre_h3 = document.querySelector(".section-sobre__explicacion h3");
 const section_sobre_p = document.querySelectorAll(".section-sobre__explicacion p");
@@ -16,8 +17,6 @@ const section_sobre_p = document.querySelectorAll(".section-sobre__explicacion p
 const section_tecnologia = document.querySelector("#section-tecnologia");
 const section_tecnologia_h3 = document.querySelector(".section-tecnologia__skills h3");
 const section_tecnologia_p = document.querySelectorAll(".section-tecnologia_grid-container__element p ");
-
-
 
 const section_trabajo = document.querySelector("#section-trabajo");
 const section_trabajo_h3 = document.querySelector("#section-trabajo h3");
@@ -27,7 +26,6 @@ const section_trabajo_pacman_body = document.querySelector(".pacman-body");
 const section_trabajo_pacman_mouth = document.querySelector(".pacman-mouth .pacman");
 const section_trabajo_circuloG = document.querySelectorAll(".circulo");
 const section_trabajo_circuloP = document.querySelectorAll(".circulo-pequeño");
-
 
 const section_contacto = document.querySelector("#section-contacto");
 const section_contacto_h3 = document.querySelector("#section-contacto h3");
@@ -40,165 +38,37 @@ const footer = document.querySelector(".footer");
 const footer_p = document.querySelector(".footer p");
 const footer_solid = document.querySelector(".solid");
 
-
-
 var reproducir = new Audio();
 reproducir.src = "audio/cafetera.mp3";
 
-const colorBlanco = "#f8f4e1";
-const colorMarron = "#4e3620";
-const colorMarroncete = "#897853";
 
-const cafes = [{
-    nombre: "EXPRESSO",
-    url: "img/cafes/cafe-solo.png",
-    url2: "img/cafes/cafe-solo2.png"
-}, {
-    nombre: "LATTE",
-    url: "img/cafes/capuccino.png",
-    url2: "img/cafes/capuccino-2.png"
-}, {
-    nombre: "IRLANDÉS",
-    url: "img/cafes/irlandes.png",
-    url2: "img/cafes/irlandes-2.png"
-}, {
-    nombre: "LECHE",
-    url: "img/cafes/leche.png",
-    url2: "img/cafes/leche-2.png"
-}]
+let contCoffe = 0
+let cafes = [];
+let colores = [];
 
-const colores = [{
-    "nombre": "EXPRESSO",
-    "colores_sections": [{
-            "colB": colorMarron,
-            "colH3": colorBlanco,
-            "colP": colorBlanco
-        },
-        {
-            "colB": colorMarron,
-            "colH3": colorBlanco,
-            "colP": colorBlanco
-        },
-        {
-            "colB": colorMarron,
-            "colH3": colorBlanco,
-            "colP_A": colorBlanco,
-            "colP_T": colorBlanco,
-            "colPac": colorBlanco,
-            "colPacM": colorMarron
-        },
-        {
-            "colB": colorMarron,
-            "colH3": colorBlanco,
-            "colA": colorBlanco,
-            "colP": colorBlanco
-        },
-        {
-            "colB": colorBlanco,
-            "colP": colorMarron,
-            "colS": colorMarron,
-        }
-    ]
-}, {
-    "nombre": "LATTE",
-    "colores_sections": [{
-            "colB": colorBlanco,
-            "colH3": colorMarron,
-            "colP": colorMarron
-        },
-        {
-            "colB": colorBlanco,
-            "colH3": colorMarron,
-            "colP": colorMarron
-        },
-        {
-            "colB": colorMarron,
-            "colH3": colorBlanco,
-            "colP_A": colorBlanco,
-            "colP_T": colorBlanco,
-            "colPac": colorBlanco,
-            "colPacM": colorMarron
-        },
-        {
-            "colB": colorMarron,
-            "colH3": colorBlanco,
-            "colA": colorBlanco,
-            "colP": colorBlanco
-        },
-        {
-            "colB": colorBlanco,
-            "colP": colorMarron,
-            "colS": colorMarron,
-        }
-    ]
-}, {
-    "nombre": "IRLANDÉS",
-    "colores_sections": [{
-            "colB": colorMarroncete,
-            "colH3": colorBlanco,
-            "colP": colorBlanco
-        },
-        {
-            "colB": colorMarroncete,
-            "colH3": colorBlanco,
-            "colP": colorBlanco
-        },
-        {
-            "colB": colorMarron,
-            "colH3": colorBlanco,
-            "colP_A": colorBlanco,
-            "colP_T": colorBlanco,
-            "colPac": colorBlanco,
-            "colPacM": colorMarron
-        },
-        {
-            "colB": colorMarron,
-            "colH3": colorBlanco,
-            "colA": colorBlanco,
-            "colP": colorBlanco
-        },
-        {
-            "colB": colorBlanco,
-            "colP": colorMarron,
-            "colS": colorMarron,
-        }
-    ]
-}, {
-    "nombre": "LECHE",
-    "colores_sections": [{
-            "colB": colorBlanco,
-            "colH3": colorMarron,
-            "colP": colorMarron
-        },
-        {
-            "colB": colorBlanco,
-            "colH3": colorMarron,
-            "colP": colorMarron
-        },
-        {
-            "colB": colorBlanco,
-            "colH3": colorMarron,
-            "colP_A": colorMarron,
-            "colP_T": colorMarron,
-            "colPac": colorMarron,
-            "colPacM": colorBlanco
-        },
-        {
-            "colB": colorBlanco,
-            "colH3": colorMarron,
-            "colA": colorMarron,
-            "colP": colorMarron
-        },
-        {
-            "colB": colorMarron,
-            "colP": colorBlanco,
-            "colS": colorBlanco,
-        }
-    ]
-}]
+const cargarCafes = async(documentUrl) => {
+    await fetch(documentUrl)
+        .then((res) =>
+            res.json())
+        .then((res) => {
+            cafes = res.cafes;
+        })
+        .catch(() => console.log(`Document ${documentUrl} not found.`))
+}
 
+const cargarColores = async(documentUrl) => {
+    await fetch(documentUrl)
+        .then((res) =>
+            res.json())
+        .then((res) => {
+            colores = res.colores;
+        })
+        .catch(() => console.log(`Document ${documentUrl} not found.`))
+}
 
-var cont = 0
+cargarCafes("js/cafes.txt");
+cargarColores("js/colores-sections.txt");
+
 
 cafe_boton.addEventListener("click", () => {
     if (nav.style.display === "none") {
@@ -211,71 +81,69 @@ cafe_boton.addEventListener("click", () => {
     }
 });
 
+
+const changeColorSection = (section, h3, color, p = undefined) => {
+    section.style.background = color.colB;
+    section.style.border = `thick solid ${color.colB}`;
+    h3.style.color = color.colH3;
+    if (p !== undefined) {
+        for (let x = 0; x < p.length; x++) {
+            p[x].style.color = color.colP;
+        }
+    }
+}
+
+const changePacman = (color) => {
+    section_trabajo_pacman_body.style.background = color.colPac;
+    section_trabajo_pacman_mouth.style.background = color.colPacM;
+    section_trabajo_pacman_mouth.style.border = "thick solid " + color.colPacM;
+
+    for (let x = 0; x < section_trabajo_circuloG.length; x++) {
+        section_trabajo_circuloG[x].style.background = color.colPac;
+    }
+
+    for (let x = 0; x < section_trabajo_circuloP.length; x++) {
+        section_trabajo_circuloP[x].style.background = color.colPac;
+    }
+}
+
+const changeCoffeInMachine = (cafe) => {
+    cafeHecho.style.backgroundImage = `url("${cafe.url}")`;
+    imagen_boton.style.backgroundImage = `url("${cafe.url}")`;
+    cafeHechoTitulo.textContent = cafe.nombre;
+}
+
 //SELECCIONAR CAFE
 cafeHecho.addEventListener("click", () => {
     nav.style.display = "none";
     navContenedor.style.background = "none";
     cafe_boton.style.display = "flex";
 
-    section_sobre.style.background = colores[cont].colores_sections[0].colB;
-    section_sobre.style.border = "thick solid " + colores[cont].colores_sections[0].colB;
-    section_sobre_h3.style.color = colores[cont].colores_sections[0].colH3;
-    for (var x = 0; x < section_sobre_p.length; x++) {
-        section_sobre_p[x].style.color = colores[cont].colores_sections[0].colP;
+    changeColorSection(section_sobre, section_sobre_h3, colores[contCoffe].colores_sections[0], section_sobre_p);
+
+    changeColorSection(section_tecnologia, section_tecnologia_h3, colores[contCoffe].colores_sections[1], section_tecnologia_p);
+
+    changeColorSection(section_trabajo, section_trabajo_h3, colores[contCoffe].colores_sections[2], section_trabajo_p_anio);
+
+    for (let x = 0; x < section_trabajo_p_texto.length; x++) {
+        section_trabajo_p_texto[x].style.color = colores[contCoffe].colores_sections[2].colP;
+    }
+    changePacman(colores[contCoffe].colores_sections[2]);
+
+
+    changeColorSection(section_contacto, section_contacto_h3, colores[contCoffe].colores_sections[3], section_contacto_p);
+
+    for (let x = 0; x < section_contacto_a.length; x++) {
+        section_contacto_a[x].style.color = colores[contCoffe].colores_sections[3].colA;
     }
 
-    section_tecnologia.style.background = colores[cont].colores_sections[1].colB;
-    section_tecnologia.style.border = "thick solid " + colores[cont].colores_sections[1].colB;
-    section_tecnologia_h3.style.color = colores[cont].colores_sections[1].colH3;
-
-    for (var x = 0; x < section_tecnologia_p.length; x++) {
-        section_tecnologia_p[x].style.color = colores[cont].colores_sections[1].colP;
+    for (let x = 0; x < section_contacto_i.length; x++) {
+        section_contCoffeacto_i[x].style.color = colores[contCoffe].colores_sections[3].colA;
     }
 
-
-    section_trabajo.style.background = colores[cont].colores_sections[2].colB;
-    section_trabajo.style.border = "thick solid " + colores[cont].colores_sections[2].colB;
-    section_trabajo_h3.style.color = colores[cont].colores_sections[2].colH3;
-
-    for (var x = 0; x < section_trabajo_p_anio.length; x++) {
-        section_trabajo_p_anio[x].style.color = colores[cont].colores_sections[2].colP_A;
-    }
-
-    for (var x = 0; x < section_trabajo_p_texto.length; x++) {
-        section_trabajo_p_texto[x].style.color = colores[cont].colores_sections[2].colP_T;
-    }
-    section_trabajo_pacman_body.style.background = colores[cont].colores_sections[2].colPac;
-    section_trabajo_pacman_mouth.style.background = colores[cont].colores_sections[2].colPacM;
-    section_trabajo_pacman_mouth.style.border = "thick solid " + colores[cont].colores_sections[2].colPacM;
-
-    for (var x = 0; x < section_trabajo_circuloG.length; x++) {
-        section_trabajo_circuloG[x].style.background = colores[cont].colores_sections[2].colPac;
-    }
-
-    for (var x = 0; x < section_trabajo_circuloP.length; x++) {
-        section_trabajo_circuloP[x].style.background = colores[cont].colores_sections[2].colPac;
-    }
-
-    section_contacto.style.background = colores[cont].colores_sections[3].colB;
-    section_contacto.style.border = "thick solid " + colores[cont].colores_sections[3].colB;
-    section_contacto_h3.style.color = colores[cont].colores_sections[3].colH3;
-    section_contacto_boton.style.color = colores[cont].colores_sections[3].colB + " !important";
-
-    for (var x = 0; x < section_contacto_a.length; x++) {
-        section_contacto_a[x].style.color = colores[cont].colores_sections[3].colA;
-    }
-    for (var x = 0; x < section_contacto_p.length; x++) {
-        section_contacto_p[x].style.color = colores[cont].colores_sections[3].colP;
-    }
-
-    for (var x = 0; x < section_contacto_i.length; x++) {
-        section_contacto_i[x].style.color = colores[cont].colores_sections[3].colA;
-    }
-
-
-    footer.style.background = colores[cont].colores_sections[4].colB;
-    footer_p.style.color = colores[cont].colores_sections[4].colP;
-    footer_solid.style.background = colores[cont].colores_sections[4].colS;
+    footer.style.background = colores[contCoffe].colores_sections[4].colB;
+    footer_p.style.color = colores[contCoffe].colores_sections[4].colP;
+    footer_solid.style.background = colores[contCoffe].colores_sections[4].colS;
 
     //Reproduce el sonido de la máquina
     reproducir.volume = 0.5;
@@ -284,31 +152,12 @@ cafeHecho.addEventListener("click", () => {
 
 //MODIFICAR AL CAFE DE LA IZQUIERDA
 cafeIzq.addEventListener("click", () => {
-    if (cont == 0)
-        cont = 3;
-    else
-        cont--;
-
-    cafeHecho.style.backgroundImage = "url(\"" + cafes[cont].url + "\")";
-    imagen_boton.style.backgroundImage = "url(\"" + cafes[cont].url + "\")";
-    imagen_boton.onmouseover = function() {
-        this.style.backgroundImage = "url(\"" + cafes[cont].url2 + "\")";
-    }
-    cafeHechoTitulo.textContent = cafes[cont].nombre;
+    (contCoffe <= 0) ? contCoffe = 3: contCoffe--;
+    changeCoffeInMachine(cafes[contCoffe]);
 });
 
 //MODIFICAR AL CAFE DE LA DERECHA
 cafeDer.addEventListener("click", () => {
-    if (cont == 3)
-        cont = 0;
-    else
-        cont++;
-
-    cafeHecho.style.backgroundImage = "url(\"" + cafes[cont].url + "\")";
-
-    imagen_boton.style.backgroundImage = "url(\"" + cafes[cont].url + "\")";
-    imagen_boton.onmouseover = function() {
-        this.style.backgroundImage = "url(\"" + cafes[cont].url2 + "\")";
-    }
-    cafeHechoTitulo.textContent = cafes[cont].nombre;
+    contCoffe = (contCoffe + 1) % 4;
+    changeCoffeInMachine(cafes[contCoffe]);
 });
