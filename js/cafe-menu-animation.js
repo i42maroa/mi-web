1530bf0e13f8dddc39753ec958fbcaeebde2b56f
i42maroa <i42maroa@uco.es>
2021-05-13 21:58:45 +0200

    mejora completa página. Version 2

--- js/cafe-menu-animation.js
+++ js/cafe-menu-animation.js
@@ -18,12 +18,27 @@ const section_sobre_masInfo = document.querySelector(".section-about__text p b")
 
 const section_skill = document.querySelector("#section-skills");
 const section_skill_h2 = document.querySelector(".section-skills h2");
+const section_skill_h3 = document.querySelectorAll(".section-skills__container-skills h3");
 const section_skill_p = document.querySelectorAll(".section-skills__list-skills li p");
+const section_skill_masInfo = document.querySelector(".section-skills p b");
+
+
 
 const section_trabajo = document.querySelector("#section-experience");
 const section_trabajo_h2 = document.querySelector("#section-experience h2");
 const section_trabajo_p_anio = document.querySelectorAll(".section-experience-lifeline-element h3");
-const section_trabajo_p_texto = document.querySelectorAll(".section-experience-lifeline-text h4");
+const section_trabajo_h4_texto = document.querySelectorAll(".section-experience-lifeline-achieve-show h4");
+
+const section_experience_lifeline_achieve_show = document.querySelectorAll(".section-experience-lifeline-achieve-show");
+
+const section_experience_moreInfo = document.querySelectorAll(".section-experience-lifeline-achieve-show h5");
+
+const section_experience_text_show = document.querySelectorAll(".section-experience-lifeline-achieve-show h4");
+const section_experience_text_hidden = document.querySelectorAll(".section-experience-lifeline-achieve-hidden");
+
+const section_experience_text_hidden_h4 = document.querySelectorAll(".section-experience-lifeline-achieve-hidden h4");
+const section_experience_text_hidden_a = document.querySelectorAll(".section-experience-lifeline-achieve-hidden a");
+
 const section_trabajo_pacman_body = document.querySelector(".pacman-body");
 const section_trabajo_pacman_mouth = document.querySelector(".pacman-mouth .pacman");
 const section_trabajo_circuloG = document.querySelectorAll(".section-experience-lifeline-Circule");
@@ -42,7 +57,6 @@ const footer_solid = document.querySelector(".footer-solid");
 
 var reproducir = new Audio();
 reproducir.src = "audio/cafetera.mp3";
-
 
 let contCoffe = 0
 let cafes = [];
@@ -137,14 +151,27 @@ cafeHecho.addEventListener("click", () => {
     changeColorSection(section_sobre, section_sobre_h2, colores[contCoffe].colores_sections[0], section_sobre_p);
 
     changeColorSection(section_skill, section_skill_h2, colores[contCoffe].colores_sections[1], section_skill_p);
+    for (let x = 0; x < section_skill_h3.length; x++) {
+        section_skill_h3[x].style.color = colores[contCoffe].colores_sections[1].colH2;
+    }
+
+
+    section_skill_masInfo.style.color = colores[contCoffe].colores_sections[1].colH2;
 
     changeColorSection(section_trabajo, section_trabajo_h2, colores[contCoffe].colores_sections[2], section_trabajo_p_anio);
 
-    for (let x = 0; x < section_trabajo_p_texto.length; x++) {
-        section_trabajo_p_texto[x].style.color = colores[contCoffe].colores_sections[2].colP;
+    for (let x = 0; x < section_trabajo_h4_texto.length; x++) {
+        section_trabajo_h4_texto[x].style.color = colores[contCoffe].colores_sections[2].colP;
     }
     changePacman(colores[contCoffe].colores_sections[2]);
 
+    for (let x = 0; x < section_experience_text_hidden.length; x++) {
+        section_experience_text_hidden[x].style.background = colores[contCoffe].colores_sections[2].colP;
+        for (let x = 0; x < section_experience_text_hidden_h4.length; x++) {
+            section_experience_text_hidden_h4[x].style.color = colores[contCoffe].colores_sections[2].colB;
+            section_experience_text_hidden_a[x].style.color = colores[contCoffe].colores_sections[2].colB;
+        }
+    }
 
     changeColorSection(section_contacto, section_contacto_h2, colores[contCoffe].colores_sections[3], section_contacto_p);
 
@@ -166,15 +193,12 @@ cafeHecho.addEventListener("click", () => {
 });
 
 
-
-//MODIFICAR AL CAFE DE LA IZQUIERDA
 cafeIzq.addEventListener("click", () => {
     (contCoffe <= 0) ? contCoffe = 3: contCoffe--;
     changeCoffeInMachine(cafes[contCoffe]);
 
 });
 
-//MODIFICAR AL CAFE DE LA DERECHA
 cafeDer.addEventListener("click", () => {
     contCoffe = (contCoffe + 1) % 4;
     changeCoffeInMachine(cafes[contCoffe]);
@@ -190,3 +214,28 @@ section_sobre_masInfo.addEventListener("click", () => {
         second[x].style.animation = "aparecerText 1s forwards";
     }
 })
+
+section_skill_masInfo.addEventListener("click", () => {
+    const containerPrincipal = document.querySelector(".section-skills__container-skills-principal");
+    const containerSecundary = document.querySelector(".section-skills__container-skills-secondary");
+    if (containerPrincipal.style.display == "none") {
+        containerSecundary.style.display = "none";
+        containerPrincipal.style.display = "flex";
+        containerPrincipal.style.animation = "aparecerText 1s forwards ";
+    } else {
+        containerPrincipal.style.display = "none";
+        containerSecundary.style.display = "flex";
+        containerSecundary.style.animation = "aparecerText 1s forwards ";
+    }
+})
+
+
+for (let x = 0; x < section_experience_moreInfo.length; x++) {
+    section_experience_moreInfo[x].addEventListener("click", () => {
+        section_experience_lifeline_achieve_show[x].style.animation = "desaparecerRect 0.3s forwards";
+        section_experience_text_hidden[x].style.animation = "aparecerRect 0.4s 0.1s forwards";
+
+        section_experience_lifeline_achieve_show[x].style.display = "none";
+        section_experience_text_hidden[x].style.display = "flex";
+    })
+}
--- js/observer.js
+++ js/observer.js
@@ -1,21 +1,24 @@
 "use strict";
 
-const visibilityHeader = (entry) => {
-    if (!entry[0].isIntersecting) {
-        document.querySelector(".navbar-hidden__button").style.display = "flex";
-        document.querySelector(".navbar-hidden__button").style.animation = "aparecer 0.5s forwards";
-    } else {
-        document.querySelector(".navbar-hidden__button").style.animation = "desaparecer 0.5s forwards";
-        setTimeout(() => document.querySelector(".navbar-hidden__button").style.display = "none", 800);
-    }
-}
+let pacmanAnimation = 0;
+let navbarShow = false;
 
 
 const visibilitySectionAbout = (entry) => {
     if (entry[0].isIntersecting) {
         document.querySelector(".section-about__img img").style.animation = "aparecerImage 1s forwards cubic-bezier(0, 0, 0.93, 0.14)";
         document.querySelector(".section-about__text h2").style.animation = "aparecerHead 0.8s forwards";
-        document.querySelector(".section-about__text-principal").style.animation = "aparecerText 0.5s 1s forwards";
+        document.querySelector(".section-about__text-principal p").style.animation = "aparecerText 0.5s 1s forwards";
+
+        if (!navbarShow) {
+            document.querySelector(".navbar-hidden__button").style.display = "flex";
+            document.querySelector(".navbar-hidden__button").style.animation = "aparecer 0.5s forwards";
+            navbarShow = false;
+        } else {
+            document.querySelector(".navbar-hidden__button").style.animation = "desaparecer 0.5s forwards";
+            setTimeout(() => document.querySelector(".navbar-hidden__button").style.display = "none", 800);
+            navbarShow = true;
+        }
     }
 }
 
@@ -24,69 +27,85 @@ const visibilitySectionSkills = (entry) => {
         document.querySelector(".section-skills-image img").style.animation = "aparecerImage2 1s forwards cubic-bezier(0, 0, 0.93, 0.14) ";
         document.querySelector(".section-skills h2").style.animation = "aparecerHead 0.8s forwards ";
         document.querySelector(".section-skills__container-skills").style.animation = "aparecerText 0.5s 1s forwards ";
+        document.querySelector(".section-skills p b").style.animation = "aparecerText 0.5s 1s forwards ";
+
     }
 }
 
 const visibilitySectionExperience = (entry) => {
-    if (entry[0].isIntersecting) {
+    if (entry[0].isIntersecting && pacmanAnimation == 0) {
         document.querySelector("#section-experience h2").style.animation = "aparecerHead 0.8s forwards";
 
         const year = document.querySelectorAll(".section-experience-lifeline-year");
         year[0].style.animation = "aparecerText 1s forwards";
-        year[2].style.animation = "aparecerText 1s 3s forwards";
+        year[2].style.animation = "aparecerText 1s 3.5s forwards";
 
-        const achieve = document.querySelectorAll(".section-experience-lifeline-text");
+        const achieve = document.querySelectorAll(".section-experience-lifeline-achieve");
         achieve[0].style.animation = "aparecerText 1s forwards";
         achieve[1].style.animation = "aparecerText 1s 1.5s forwards";
-        achieve[2].style.animation = "aparecerText 1s 3s forwards";
-        achieve[3].style.animation = "aparecerText 1s 4.5s forwards";
+        achieve[2].style.animation = "aparecerText 1s 3.5s forwards";
+        achieve[3].style.animation = "aparecerText 1s 5.5s forwards";
 
         const circulos = document.querySelectorAll(".section-experience-lifeline-tinyCircule, .section-experience-lifeline-Circule");
-        circulos[0].style.animation = "desaparecer 0.5s 0.8s forwards";
-        circulos[1].style.animation = "desaparecer 0.5s 1.5s forwards";
-        circulos[2].style.animation = "desaparecer 0.5s 2.3s forwards";
-        circulos[3].style.animation = "desaparecer 0.5s 3s forwards";
-        circulos[4].style.animation = "desaparecer 0.5s 3.8s forwards";
-        circulos[5].style.animation = "desaparecer 0.5s 4.5s forwards";
+        circulos[0].style.animation = "desaparecer 0.3s forwards";
+        circulos[1].style.animation = "desaparecer 0.3s 0.8s forwards";
+        circulos[2].style.animation = "desaparecer 0.3s 1.5s forwards";
+        circulos[3].style.animation = "desaparecer 0.3s 2.5s forwards";
+        circulos[4].style.animation = "desaparecer 0.3s 3.5s forwards";
+        circulos[5].style.animation = "desaparecer 0.3s 4.5s forwards";
+        circulos[6].style.animation = "desaparecer 0.3s 5.5s forwards";
 
-        document.querySelector(".pacman-holder").style.animation = "move 6s linear 1 forwards";
+        setTimeout(() => {
+            circulos[0].style.animation = "aparecer 0.5s  forwards";
+            circulos[2].style.animation = "aparecer 0.5s  forwards";
+            circulos[4].style.animation = "aparecer 0.5s forwards";
+            circulos[6].style.animation = "aparecer 0.5s  forwards";
+        }, 7000);
 
+        document.querySelector(".pacman-holder").style.animation = "move 7s linear 1 forwards";
+        pacmanAnimation++;
+
+        if (!navbarShow) {
+            document.querySelector(".navbar-hidden__button").style.display = "flex";
+            document.querySelector(".navbar-hidden__button").style.animation = "aparecer 0.5s forwards";
+            navbarShow = false;
+        } else {
+            document.querySelector(".navbar-hidden__button").style.animation = "desaparecer 0.5s forwards";
+            setTimeout(() => document.querySelector(".navbar-hidden__button").style.display = "none", 800);
+            navbarShow = true;
+        }
     }
 }
 
 const visibilitySectionContact = (entry) => {
     if (entry[0].isIntersecting) {
 
-        document.querySelector(".section-contact__content h2").style.animation = "aparecerHead 0.8s forwards ";
-        document.querySelector(".section-contact__content p").style.animation = "aparecerText 1s 1s forwards ";
-
-        document.querySelector(".section-contact_button").style.animation = "aparecerText 1s 2s forwards ";
+        document.querySelector("#section-contact h2").style.animation = "aparecerHead 0.8s forwards ";
 
         const red = document.querySelectorAll(".section-contact_social-element");
-        red[0].style.animation = "aparecerText 1s 3s forwards";
-        red[1].style.animation = "aparecerText 1s 3s forwards";
-        red[2].style.animation = "aparecerText 1s 3s forwards";
+        red[0].style.animation = "aparecerImage 0.8s 0.5s forwards cubic-bezier(0, 0, 0.93, 0.14)";
+        red[1].style.animation = "aparecerImage 0.8s 0.5s forwards cubic-bezier(0, 0, 0.93, 0.14)";
+        red[2].style.animation = "aparecerImage 0.8s 0.5s forwards cubic-bezier(0, 0, 0.93, 0.14)";
 
         const redP = document.querySelectorAll(".section-contact_social-element p");
-        redP[0].style.animation = "aparecerText 1s 4s forwards";
-        redP[1].style.animation = "aparecerText 1s 4s forwards";
-        redP[2].style.animation = "aparecerText 1s 4s forwards";
+        redP[0].style.animation = "aparecerText 0.5s 1s forwards";
+        redP[1].style.animation = "aparecerText 0.5s 1s forwards";
+        redP[2].style.animation = "aparecerText 0.5s 1s forwards";
+
+        document.querySelector(".section-contact_button").style.animation = "aparecerText 1s 1s forwards ";
+
+        document.querySelector("#section-contact p").style.animation = "aparecerText 1s 2s forwards ";
     }
 }
 
-
-const observer = new IntersectionObserver(visibilityHeader, { threshold: 0.20 });
-observer.observe(document.querySelector("header"));
-
-
-const observerSectionAbout = new IntersectionObserver(visibilitySectionAbout, { threshold: 0.6 });
+const observerSectionAbout = new IntersectionObserver(visibilitySectionAbout, { threshold: 0.4 });
 observerSectionAbout.observe(document.querySelector("#section-about"));
 
-const observerSectionSkills = new IntersectionObserver(visibilitySectionSkills, { threshold: 0.6 });
+const observerSectionSkills = new IntersectionObserver(visibilitySectionSkills, { threshold: 0.4 });
 observerSectionSkills.observe(document.querySelector("#section-skills"));
 
-const observerSectionExperience = new IntersectionObserver(visibilitySectionExperience, { threshold: 0.6 });
+const observerSectionExperience = new IntersectionObserver(visibilitySectionExperience, { threshold: 0.4 });
 observerSectionExperience.observe(document.querySelector("#section-experience"));
 
-const observerSectionContact = new IntersectionObserver(visibilitySectionContact, { threshold: 0.6 });
+const observerSectionContact = new IntersectionObserver(visibilitySectionContact, { threshold: 0.4 });
 observerSectionContact.observe(document.querySelector("#section-contact"));
